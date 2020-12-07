ce2d9e9ae1e72a50ad201a2480957ce044aa6554
xhiron <belgradista@yahoo.de>
2020-12-07 11:28:49 +0100

    component style

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Directory } from "../../components/directory/Directory"
+import "./homepage.scss"
+
+export const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+)
