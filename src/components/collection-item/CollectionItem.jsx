460866d6da4aa5d3d34950f939bac9a262ffec9c
xhiron <belgradista@yahoo.de>
2020-12-08 12:58:49 +0100

    updated shop data

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -1,14 +1,27 @@
 import React from "react"
+import CustomButton from "../custom-button/CustomButton"
+import { connect } from "react-redux"
 import "./CollectionItem.scss"
+import { addItem } from "../../redux/cart/cart.action"
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <div className="price">{price}</div>
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <div className="price">{price}</div>
+      </div>
+      <CustomButton inverted onClick={() => addItem(item)}>
+        ADD TO CART
+      </CustomButton>
     </div>
-  </div>
-)
+  )
+}
 
-export default CollectionItem
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
--- src/components/custom-button/CustomButton.jsx
+++ src/components/custom-button/CustomButton.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import "./CustomButton.scss"
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <div className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+  <div className={`${inverted ? "google-sign-in" : ""} ${isGoogleSignIn ? "google-sign-in" : ""} custom-button`} {...otherProps}>
     {children}
   </div>
 )
